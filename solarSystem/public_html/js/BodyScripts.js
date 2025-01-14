9e55fc5876c0836fc3b1825b46cd62925b63427f
zafericen <zafericen1907@gmail.com>
2025-01-15 01:49:00 +0300

    Testing

--- solarSystem/public_html/js/BodyScripts.js
+++ solarSystem/public_html/js/BodyScripts.js
@@ -700,12 +700,13 @@ class CameraFollowerScript extends SceneObjectScript{
     }
 
     Update() {
-        if(this.targetBody)
-        this.camera.target = this.targetBody.transform.position;
+        if(this.targetBody && this.targetObject !== "none")
+        this.camera.target = vec3.copy(this.targetBody.transform.position);
         if (this.targetObject === "spaceship") {
-            this.updateSpaceShipCameraVectors()
+            this.updateSpaceShipCameraVectors();
         } else {
-            this.camera.updateCameraVectors()
+            this.camera.updateCameraVectors();
+
         }
     }
 
